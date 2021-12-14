32f6086bb67b1d0465ecbd644c1181716e0124b2
comeRodriguez <85586434+comeRodriguez@users.noreply.github.com>
2021-12-14 19:32:07 +0100

    Create deep neural network model from scratch

---
+++ 1 - neural_network_and_deep_learning/deep_neural_network_model.py
@@ -0,0 +1,41 @@
+import numpy as np
+from building_deep_neural_network_from_scratch import (
+    initialize_parameters_deep,
+    L_model_forward,
+    compute_cost,
+    L_model_backward,
+    update_parameters
+)
+
+
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+
+    Arguments:
+    X -- data, numpy array of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+
+    np.random.seed(1)
+    costs = []                         # keep track of cost
+
+    parameters = initialize_parameters_deep(layer_dims=layers_dims)
+    for i in range(0, num_iterations):
+        AL, caches = L_model_forward(X=X, parameters=parameters)
+        cost = compute_cost(AL, Y)
+        grads = L_model_backward(AL, Y, caches)
+        parameters = update_parameters(parameters, grads, learning_rate)
+        if print_cost and i % 100 == 0 or i == num_iterations - 1:
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if i % 100 == 0 or i == num_iterations:
+            costs.append(cost)
+
+    return parameters, costs
